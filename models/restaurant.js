4a5de08e74f34b7a02d0c076874d5d14fae479ff
jessie758 <jessie272758@gmail.com>
2024-03-24 22:54:50 +0800

    建立 Restaurant 資料表

---
+++ models/restaurant.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Restaurant extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Restaurant.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      tel: DataTypes.STRING,
+      openingHours: DataTypes.STRING,
+      address: DataTypes.STRING,
+      description: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: 'Restaurant',
+      tableName: 'Restaurants',
+      underscored: true,
+    }
+  );
+  return Restaurant;
+};
